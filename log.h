fd6c0b41f85c626502ce3a1c607cfe7d50900c97
Alex <alexanderw9511@gmail.com>
2026-05-12 20:52:24 +0800

    Major revamp of the logging functions and logging styling

--- log.h
+++ log.h
@@ -1,58 +1,106 @@
 #ifndef LOG_H_
-
-int l_init();
-/*
-* Looks for and creates a 'logs' directory where log files are placed.
-* Logs are named in the format: "YYYY-MM-DD--HH:MM:SS.log"
-*/
-
-int l_close();
-/*
-* Close the log file.
-* Call before end of program.
-*/
-
-#define l_log(dest, flag, msg) \
-    _l_log(__FILE__, __LINE__, dest, flag, msg)
-/*
-* Writes into the logfile at a given time the program is ran.
-* Dest determines whether to write to stdout or a file
-* Dest includes:
-*   - STDOUT_FILENO -> write to stdout
-*   - STDERR_FILENO - write to stderr
-*   - FILE_FILENO - write to file
-* Flags gives nicer formatting of messages.
-* Flags include:
-*   - INFO
-*   - ERR
-*/
-
-#define l_flog(dest, flag, msg, ...) \
-    _l_flog(__FILE__, __LINE__, dest, flag, msg, __VA_ARGS__)
-/*
-* Formatted logging.
-* Otherwise same as l_log.
-*/
-
-void _l_flog(char *file, int line, int dest, int flag, const char *msg, ...);
-void _l_log(char *file, int line, int dest, int flag, char *msg);
-
 #include <string.h>
 #include <time.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdarg.h>
-
-#define INFO 0
-#define ERR 1
-
-#define FILE_FILENO 3
+#include <stdlib.h>
 
 
-FILE *lgfd;
+#define LOG_INIT FILE *LGFD = NULL; // put LOG_INIT at the beginning of the file
 
-int l_init() {
+/*
+* Looks for and creates a 'logs' directory where log files are placed.
+* Logs are named in the format: "YYYY-MM-DD--HH:MM:SS.log"
+* Required at the beginning of the program
+*/
+int l_init();
+
+/*
+* Close the log file.
+* Call before end of program.
+*/
+int l_close();
+
+/*
+* Writes into the logfile and one of the STD ouutputs
+* Flags determines which STD output the message is sent to in addition to the logfile
+* Flags include:
+*   - INFO
+*   - ERR
+*/
+#define l_log(flag, msg) \
+    __write_two(__FILE__, __LINE__, flag, msg)
+
+/*
+* Formatted logging.
+* Otherwise same as log.
+*/
+#define l_logf(flag, msg, ...) \
+    __write_two_f(__FILE__, __LINE__, flag, msg, __VA_ARGS__)
+
+
+/*
+* Writes only to file
+*/
+#define l_flog(flag, msg) \
+    __write_file(__FILE__, __LINE__, flag, msg)
+
+/*
+* Formatted write to file
+*/
+#define l_flogf(flag, msg, ...) \
+    __write_file_f(__FILE__, __LINE__, flag, msg, __VA_ARGS__)
+
+
+/*
+* Writes only to stdout
+*/
+#define l_olog(flag, msg) \
+    __write_out(__FILE__, __LINE__, flag, msg)
+
+/*
+* Formatted write to stdout
+*/
+#define l_ologf(flag, msg, ...) \
+    __write_out_f(__FILE__, __LINE__, flag, msg, __VA_ARGS__)
+
+
+/*
+* Writes only to stderr
+*/
+#define l_elog(flag, msg) \
+    __write_err(__FILE__, __LINE__, flag, msg)
+
+/*
+* Formatted write to stderr
+*/
+#define l_elogf(flag, msg, ...) \
+    __write_err_f(__FILE__, __LINE__, flag, msg, __VA_ARGS__)
+
+
+static void __write_two(char *file, int line, int flag, char *msg);
+static void __write_two_f(char *file, int line, int flag, const char *msg, ...);
+static void __write_one(char *file, int line, FILE* dest, int flag, char *msg);
+static void __write_one_f(char *file, int line, FILE* dest, int flag, const char *msg, va_list args);
+
+static void __write_file(char *file, int line, int flag, char *msg);
+static void __write_out(char *file, int line, int flag, char *msg);
+static void __write_err(char *file, int line, int flag, char *msg);
+static void __write_file_f(char *file, int line, int flag, const char *msg, ...);
+static void __write_out_f(char *file, int line, int flag, const char *msg, ...);
+static void __write_err_f(char *file, int line, int flag, const char *msg, ...);
+inline char* getTime();
+
+typedef enum {
+    L_INFO,
+    L_ERR,
+} Flag;
+
+extern FILE *LGFD; // log file descriptor
+
+inline int l_init() {
     // check/create /logs directory
     int mk = mkdir("./logs", 0777);
 
@@ -63,10 +111,21 @@ int l_init() {
     size_t ret = strftime(s, sizeof(s), "%F--%T", tm);
 
     // format log filename with date-time
-    char *path = "./logs/";
-    char fname[80];
+    char cwd[2048] = {0};
+    char *directory = getcwd(cwd, 2048);
+    char path[3072] = {0};
+    if (directory == NULL) {
+        // back up in case getcwd fails
+        strcpy(path, "./logs/");
+    } else {
+        strcpy(path, directory);
+        strcat(path, "/logs/");
+    }
+    char fname[3072];
+    char title[3072];
     strcpy(fname, path);
     strcat(fname, s);
+    strcpy(title, fname);
     strcat(fname, ".log");
 
     // create log file
@@ -75,64 +134,198 @@ int l_init() {
         perror("error finding/creating file\n");
         return -1;
     }
+    fprintf(fd, "------------------ %s ------------------\n\n", title);
+    fflush(fd);
 
-    lgfd = fd;
+    LGFD = fd;
     return 0;
 }
 
-void _l_log(char *file, int line, int dest, int flag, char *msg) {
+char* getTime() {
+    // get and format current time
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    char *time = (char*)malloc(64 * sizeof(char));
+    // remember to free time after use
+    size_t ret = strftime(time, sizeof(time), "%T", tm);
 
-    FILE *localfd;
+    return time;
+}
 
-    if (dest == STDOUT_FILENO) {
-        localfd = stdout;
-    } else if (dest == STDERR_FILENO) {
-        localfd = stderr;
-    } else if (dest == FILE_FILENO) {
-        localfd = lgfd;
+inline void __write_two(char *file, int line, int flag, char *msg) {
+
+    FILE *fd = LGFD;
+
+    char *time = getTime();
+
+    if (flag == L_INFO) {
+        // send to stdout
+        fprintf(fd, "[INFO]\t %s:%i %s --> %s\n", file, line, time, msg);
+        fprintf(stdout, "\033[033m[INFO]\033[0m\t %s:%i %s --> %s\n", file, line, time, msg);
+    } else if (flag == L_ERR) {
+        // send to stderr
+        fprintf(fd, "[ERROR]\t %s:%i %s --> %s\n", file, line, time, msg);
+        fprintf(stderr, "\033[031m[ERROR]\033[0m\t %s:%i %s --> %s\n", file, line, time, msg);
     }
-
-    if (flag == INFO) {
-        fprintf(localfd, "%s:%i [INFO] --> %s", file, line, msg);
-        fprintf(localfd, "\n");
-    } else if (flag == ERR) {
-        fprintf(localfd, "%s:%i [ERROR] --> %s", file, line, msg);
-        fprintf(localfd, "\n");
-    }
-    fflush(localfd);
+    fflush(fd);
+    free(time);
     return;
 }
 
-void _l_flog(char *file, int line, int dest, int flag, const char *msg, ...) {
+inline void __write_two_f(char *file, int line, int flag, const char *msg, ...) {
 
-    FILE *localfd;
-
-    if (dest == STDOUT_FILENO) {
-        localfd = stdout;
-    } else if (dest == STDERR_FILENO) {
-        localfd = stderr;
-    } else if (dest == FILE_FILENO) {
-        localfd = lgfd;
-    }
+    FILE *fd = LGFD;
+    char *time = getTime();
 
     va_list args;
     va_start(args, msg);
-    if (flag == INFO) {
-        fprintf(localfd, "%s:%i [INFO] --> ", file, line);
-        vfprintf(localfd, msg, args);
-        fprintf(localfd, "\n");
-    } else if (flag == ERR) {
-        fprintf(localfd, "%s:%i [ERROR] --> ", file, line);
-        vfprintf(localfd, msg, args);
-        fprintf(localfd, "\n");
+    if (flag == L_INFO) {
+        // send to file
+        fprintf(fd, "[INFO]\t %s:%i %s --> ", file, line, time);
+        vfprintf(fd, msg, args);
+        fprintf(fd, "\n");
+        // send to stdout
+        fprintf(stdout, "\033[033m[INFO]\033[0m\t %s:%i %s --> ", file, line, time);
+        vfprintf(stdout, msg, args);
+        fprintf(stdout, "\n");
+    } else if (flag == L_ERR) {
+        // send to file
+        fprintf(fd, "[ERROR]\t %s:%i %s --> ", file, line, time);
+        vfprintf(fd, msg, args);
+        fprintf(fd, "\n");
+        // send to stderr
+        fprintf(stderr, "\033[031m[ERROR]\033[0m\t %s:%i %s --> ", file, line, time);
+        vfprintf(stderr, msg, args);
+        fprintf(stderr, "\n");
+
     }
+
     va_end(args);
-    fflush(localfd);
+    fflush(fd);
+    free(time);
     return;
 }
 
-int l_close() {
-    if (fclose(lgfd) == EOF) {
+static void __write_one(char *file, int line, FILE* dest, int flag, char *msg) {
+
+    char *time = getTime();
+
+    if (flag == L_INFO) {
+        if (dest == stdout) {
+            // stdout
+            fprintf(stdout, "\033[033m[INFO]\033[0m\t %s:%i %s --> %s\n", file, line, time, msg);
+        } else if (dest == stderr) {
+            // stderr
+            fprintf(stderr, "\033[033m[INFO]\033[0m\t %s:%i %s --> %s\n", file, line, time, msg);
+        } else {
+            // file
+            fprintf(LGFD, "[INFO]\t %s:%i %s --> %s\n", file, line, time, msg);
+        }
+    } else if (flag == L_ERR) {
+        // send to stderr
+        if (dest == stdout) {
+            // stdout
+            fprintf(stdout, "\033[031m[ERROR]\033[0m\t %s:%i %s --> %s\n", file, line, time, msg);
+        } else if (dest == stderr) {
+            // stderr
+            fprintf(stderr, "\033[031m[ERROR]\033[0m\t %s:%i %s --> %s\n", file, line, time, msg);
+        } else {
+            // file
+            fprintf(LGFD, "[ERROR]\t %s:%i %s --> %s\n", file, line, time, msg);
+        }
+    }
+    fflush(dest);
+    free(time);
+    return;
+}
+
+inline void __write_one_f(char *file, int line, FILE* dest, int flag, const char *msg, va_list args) {
+
+    char *time = getTime();
+
+    if (flag == L_INFO) {
+        // send to stdout
+        if (dest == stdout) {
+            // stdout
+            fprintf(stdout, "\033[033m[INFO]\033[0m\t %s:%i %s --> ", file, line, time);
+            vfprintf(stdout, msg, args);
+            fprintf(stdout, "\n");
+        } else if (dest == stderr) {
+            // stderr
+            fprintf(stderr, "\033[033m[INFO]\033[0m\t %s:%i %s --> ", file, line, time);
+            vfprintf(stderr, msg, args);
+            fprintf(stderr, "\n");
+        } else {
+            // file
+            fprintf(LGFD, "[INFO]\t %s:%i %s --> ", file, line, time);
+            vfprintf(LGFD, msg, args);
+            fprintf(LGFD, "\n");
+        }
+    } else if (flag == L_ERR) {
+        // send to stderr
+       if (dest == stdout) {
+            // stdout
+            fprintf(stdout, "\033[031m[ERROR]\033[0m\t %s:%i %s --> ", file, line, time);
+            vfprintf(stdout, msg, args);
+            fprintf(stdout, "\n");
+        } else if (dest == stderr) {
+            // stderr
+            fprintf(stderr, "\033[031m[ERROR]\033[0m\t %s:%i %s --> ", file, line, time);
+            vfprintf(stderr, msg, args);
+            fprintf(stderr, "\n");
+        } else {
+            // file
+            fprintf(LGFD, "[ERROR]\t %s:%i %s --> ", file, line, time);
+            vfprintf(LGFD, msg, args);
+            fprintf(LGFD, "\n");
+        }
+    }
+    fflush(dest);
+    free(time);
+    return;
+}
+
+static void __write_file(char *file, int line, int flag, char *msg) {
+    __write_one(file, line, LGFD, flag, msg);
+    return;
+}
+
+static void __write_out(char *file, int line, int flag, char *msg) {
+    __write_one(file, line, stdout, flag, msg);
+    return;
+}
+
+static void __write_err(char *file, int line, int flag, char *msg) {
+    __write_one(file, line, stderr, flag, msg);
+    return;
+}
+
+static void __write_file_f(char *file, int line, int flag, const char *msg, ...) {
+    va_list args;
+    va_start(args, msg);
+    __write_one_f(file, line, LGFD, flag, msg, args);
+    va_end(args);
+    return;
+}
+
+static void __write_out_f(char *file, int line, int flag, const char *msg, ...) {
+    va_list args;
+    va_start(args, msg);
+    __write_one_f(file, line, stdout, flag, msg, args);
+    va_end(args);
+    return;
+}
+
+static void __write_err_f(char *file, int line, int flag, const char *msg, ...) {
+    va_list args;
+    va_start(args, msg);
+    __write_one_f(file, line, stderr, flag, msg, args);
+    va_end(args);
+    return; 
+}
+
+inline int l_close() {
+    if (fclose(LGFD) == EOF) {
         perror("Error closing file");
         return -1;
     }
--- main.c
+++ main.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
 #include "log.h"
+
+LOG_INIT
 
 int main(int argc, char *argv[]) {
 
   l_init();
 
-  l_log(FILE_FILENO, INFO, "whats up");
-  l_log(STDERR_FILENO, ERR, "error");
-  l_log(STDOUT_FILENO, INFO, "yo");
+  l_log(L_INFO, "whats up");
+  l_log(L_ERR, "error");
+  l_log(L_INFO, "yo");
 
-  l_flog(FILE_FILENO, INFO, "hi %i", 1);
-  l_flog(FILE_FILENO, INFO, "hi %i", 2);
-  l_flog(FILE_FILENO, INFO, "hi %i", 3);
-  l_flog(FILE_FILENO, INFO, "hi %i", 4);
+  l_logf(L_INFO, "hi %i", 1);
+  l_logf(L_INFO, "hi %i", 2);
+  l_logf(L_INFO, "hi %i", 3);
+  l_logf(L_INFO, "hi %i", 4);
 
   char *hi = "yo";
-  l_flog(FILE_FILENO, INFO, "hi %s", "yo hi");
-  l_flog(STDOUT_FILENO, ERR, "nah %s bruh", hi);
-  l_flog(STDERR_FILENO, INFO, "yo, %s what's up", hi);
+  l_logf(L_INFO, "hi %s", "yo hi");
+  l_logf(L_ERR, "nah %s bruh", hi);
+  l_logf(L_INFO, "yo, %s what's up", hi);
 
   printf("%i\n", fileno(stdout));
   printf("%i\n", fileno(stderr));
@@ -28,6 +28,15 @@ int main(int argc, char *argv[]) {
 
   printf("%i\n", STDOUT_FILENO);
   printf("%i\n", STDERR_FILENO);
+
+  l_flog(L_INFO, "hello");
+  l_flogf(L_ERR, "hi %s", "there");
+
+  l_olog(L_ERR, "this is a error in stdout");
+  l_ologf(L_ERR, "this is a error in stdout #%d", 2);
+
+  l_elog(L_INFO, "this is a msg in stderr");
+  l_elogf(L_INFO, "this is a msg in stderr #%d", 3);
 
   l_close();
   return 0;
